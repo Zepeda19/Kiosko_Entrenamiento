d922f56d8e7044ec2b9022d09064d4885a85f943
Zepeda19 <jzepeda6@ucol.mx>
2022-02-02 11:10:49 -0600

    Inicial

---
+++ Models/Modulos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kiosko.Models
+{
+    public class Modulos
+    {
+        [Key]
+        public int intModuloKey { get; set; }
+        public string vchTitulo { get; set; }
+        public Nullable<bool> bitAccesoDirecto { get; set; }
+        public Nullable<int> intOrden { get; set; }
+    }
+}
